ba2e49215d72911b1917360046727f85106c26cd
xSmair <simonmair01@gmx.at>
2022-12-02 09:05:44 +0100

    Initial commit

---
+++ src/main/java/io/gihub/xsmair/springbootshowcase/SpringBootShowcaseApplication.java
@@ -0,0 +1,13 @@
+package io.gihub.xsmair.springbootshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShowcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/gihub/xsmair/springbootshowcase/SpringBootShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gihub.xsmair.springbootshowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
